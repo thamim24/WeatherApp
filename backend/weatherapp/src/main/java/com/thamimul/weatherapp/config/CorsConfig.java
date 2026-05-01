ee7c0a90ae452de64286efd9d188fe0c54b130cc
Thamimul Ansari <thamimul2004@gmail.com>
2026-05-01 21:23:44 +0530

    Update CorsConfig.java

--- backend/weatherapp/src/main/java/com/thamimul/weatherapp/config/CorsConfig.java
+++ backend/weatherapp/src/main/java/com/thamimul/weatherapp/config/CorsConfig.java
@@ -2,8 +2,7 @@ package com.thamimul.weatherapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
@@ -13,16 +12,15 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/api/**")
-                        .allowedOrigins(
-                            "http://localhost:5173",
-                            "https://weather-app-pearl-eta-11.vercel.app",
-                            "https://weather-app-git-main-thamimul-ansaris-projects.vercel.app",
-                            "https://weather-m14if22vx-thamimul-ansaris-projects.vercel.app"
+                        .allowedOriginPatterns(
+                                "http://localhost:5173",
+                                "https://*.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false); // keep false unless using auth
             }
         };
     }
